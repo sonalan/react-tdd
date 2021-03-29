60b970ac0a52192b4913b08dace969cc3f523019
sonalan <sinan@onalan.net>
2021-03-29 14:28:39 -0700

    add an unordered list

--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -1,4 +1,10 @@
 import React from 'react';
 export default function RoomList() {
-
+    return (
+        <ul>
+            <li>Room 1</li>
+            <li>Room 2</li>
+            <li>Room 3</li>
+        </ul>
+    )
 }
