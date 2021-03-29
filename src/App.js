1ef703d28b67add2a14a892d74e97b3b9b61a520
sonalan <sinan@onalan.net>
2021-03-28 21:44:59 -0700

    implemented RoomList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonList from "./components/RoomList/RoomList";
 
 function App() {
   return (
     <div className="App">
-      
+      <RoomList/>
     </div>
   );
 }
