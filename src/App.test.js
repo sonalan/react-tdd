47fd5c18349ce3e211e44b83f3819a6143a691dc
sonalan <sinan@onalan.net>
2021-03-28 19:36:06 -0700

    deleted default test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,2 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-// test() or it() can be used
-it('renders learn react link', () => {
-  // render the app component
-  render(<App />);
-
-  // get element by contains specific text
-  const linkElement = screen.getByText(/learn react/i);
-
-  // check the assertion if it fulfill the conditions
-  expect(linkElement).toBeInTheDocument();
-});
