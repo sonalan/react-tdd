c05ae49020cbc2600977d581d4fd27f9951b7e26
sonalan <sinan@onalan.net>
2021-03-28 17:07:58 -0700

    basic steps of testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,12 @@ import App from './App';
 
 // test() or it() can be used
 it('renders learn react link', () => {
+  // render the app component
   render(<App />);
+
+  // get element by contains specific text
   const linkElement = screen.getByText(/learn react/i);
+
+  // check the assertion if it fulfill the conditions
   expect(linkElement).toBeInTheDocument();
 });
