849f404a457878c7346a124b4d5a61880434d8d6
sonalan <sinan@onalan.net>
2021-03-28 21:45:14 -0700

    import RoomListComponent

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import App from './App';
 import {shallow} from "enzyme";
+import RoomList from "./components/RoomList/RoomList";
 
 describe('App', ()=>{
   it('renders without crashing', ()=>{
