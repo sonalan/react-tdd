2645de16d2012f1efa556bc8fa5da01c78e96fc2
sonalan <sinan@onalan.net>
2021-03-28 23:55:40 -0700

    refactor tests

--- src/App.test.js
+++ src/App.test.js
@@ -10,26 +10,25 @@ describe('App', ()=>{
       const appWrapper = shallow(<App/>);
   });
 
+  /*
   it('renders without crashing', ()=>{
     const appWrapper = shallow(<App/>);
   })
+  */
 
   it('renders a room list ', ()=>{
-    const appWrapper = shallow(<App/>);
     const roomList = appWrapper.find(RoomList);
 
     expect(roomList).toHaveLength(1);
   });
 
   it('has state', ()=>{
-    const appWrapper = shallow(<App/>);
     const appState = appWrapper.state();
 
     expect(appState).not.toBeNull();
   })
 
   it('', ()=>{
-    const appWrapper = shallow(<App/>);
     const appState = appWrapper.state();
 
     expect(appState.rooms).toBeDefined();
