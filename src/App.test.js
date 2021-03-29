7baf11a4ad74e8996afc4e79ed943d1ee2654027
sonalan <sinan@onalan.net>
2021-03-28 20:30:47 -0700

    fist test defined that passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import {shallow} from "enzyme";
 
 describe('App', ()=>{
-
+  it('renders without crashing', ()=>{
+    const appWrapper = shallow(<App/>);
+  })
 });
