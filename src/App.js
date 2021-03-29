425d2da187ff728fe08a3fb75c15fae7fc31454f
sonalan <sinan@onalan.net>
2021-03-29 00:39:29 -0700

    defined rooms prop with state.room property

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component{
   render(){
     return (
         <div className="App">
-          <RoomList/>
+          <RoomList rooms={this.state.rooms}/>
         </div>
     );
   }
